f6d0e3111d02e15b7e142847da4a66a5b4180932
belovetech <belovetech@gmail.com>
2023-04-02 15:37:38 +0100

    [Update]: generate random room secret code

--- utils/generateSecretRoomPassCode.js
+++ utils/generateSecretRoomPassCode.js
@@ -1,24 +1,13 @@
-/* eslint-disable no-plusplus */
 const generateSecretRoomPassCode = () => {
-  const min = Math.ceil(100000);
-  const max = Math.floor(999999);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const generateSecretRoomPassCode2 = () => {
   const alphabetNumeric = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let res = '';
   const len = 6;
 
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < len; i++) {
     res += alphabetNumeric[Math.floor(Math.random() * alphabetNumeric.length)];
   }
-  res = `${res.slice(0, 3)}-${res.slice(3)}`;
-
-  return res;
+  return `${res.slice(0, 3)}-${res.slice(3)}`;
 };
 
-module.exports = {
-  generateSecretRoomPassCode,
-  generateSecretRoomPassCode2,
-};
+module.exports = generateSecretRoomPassCode;
