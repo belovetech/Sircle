0511efca1c966233ce627697544f654ee002a143
belovetech <belovetech@gmail.com>
2023-04-02 15:38:30 +0100

    [Update]: define route for the route handlers

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const AppController = require('../controllers/AppController');
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
+router.post('/room', AppController.createRoom);
+router.get('/room/:passcode', AppController.getRoom);
+router.post('/join-room', AppController.joinRoom);
 
 module.exports = router;
